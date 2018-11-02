13653a843d03642b3c7c1613eb42648ae1bd7310
mcvo <macovogaming@gmail.com>
2018-11-02 01:11:39 +0000

    Add files via upload

---
+++ precache-manifest.97dc4693fa81e1603295f718bbb92d5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df50f9b560871b6a0823",
+    "url": "/static/js/main.df50f9b5.chunk.js"
+  },
+  {
+    "revision": "0fc6f1b4b17fdee137e7",
+    "url": "/static/js/1.0fc6f1b4.chunk.js"
+  },
+  {
+    "revision": "df50f9b560871b6a0823",
+    "url": "/static/css/main.8a219e7f.chunk.css"
+  },
+  {
+    "revision": "a0a2a45384be29a02a9d5026c16fbf86",
+    "url": "/index.html"
+  }
+];
